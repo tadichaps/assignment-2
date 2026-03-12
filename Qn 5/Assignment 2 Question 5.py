34cdbf202e1eb8cd8bf2e8bda86d60e9796d48be
tadiwachaps28-ops <tadiwachaps28@gmail.com>
2026-03-12 19:22:12 +0200

    Response to question 5

---
+++ Qn 5/Assignment 2 Question 5.py
@@ -0,0 +1,18 @@
+# math_operations.py
+# This module contains basic mathematical operations
+
+# Function to add two numbers
+def add(a, b):
+    """
+    Returns the sum of two numbers
+    """
+    return a + b
+
+
+# Function to multiply two numbers
+def multiply(a, b):
+    """
+    Returns the product of two numbers
+    """
+    return a * b
+
